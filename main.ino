e6ee408163e1aa814257948d3ea5e43198fc9c9f
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-23 17:17:56 -0300

    configura credenciais WiFi e tópicos MQTT do projeto Clyvo Vet

--- main.ino
+++ main.ino
@@ -16,3 +16,17 @@
 #define PIN_BUZZER    13
 
 #define PIN_LED_REMOTO 15
+
+
+// ====================== CONFIGURAÇÕES WiFi / MQTT ======================
+const char* SSID         = "Wokwi-GUEST";
+const char* PASSWORD     = "";
+
+const char* BROKER_MQTT  = "broker.hivemq.com";
+const int   BROKER_PORT  = 1883;
+const char* ID_MQTT      = "clyvo_pet_esp32_001";
+
+// Tópicos MQTT (padronizados com namespace 'clyvo/pet')
+const char* TOPIC_PUB_TELEMETRIA = "clyvo/pet/telemetria";
+const char* TOPIC_SUB_COMANDO    = "clyvo/pet/comando";
+const char* TOPIC_PUB_ALERTA     = "clyvo/pet/alerta";
