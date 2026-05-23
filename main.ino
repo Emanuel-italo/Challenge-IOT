3d61f18f8f6d03efb2b253d3f23ed171b0c459a3
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-23 17:16:18 -0300

    define pinos para LED RGB

--- main.ino
+++ main.ino
@@ -7,3 +7,8 @@
 #define PIN_DHT       12
 #define PIN_TRIG      25
 #define PIN_ECHO      26
+
+// LED RGB (status de saúde do pet)
+#define PIN_LED_R     21
+#define PIN_LED_G     22
+#define PIN_LED_B     23
